171e55135048c6fc5890fe6f5f26dbbe0741d4ac
Grant Beyleveld <grantbey@gmail.com>
2015-10-10 14:15:51 -0400

    Digit recognition is working now.

--- digit_nn.py
+++ digit_nn.py
@@ -6,7 +6,7 @@ from sklearn.cross_validation import train_test_split
 from scipy.optimize import fmin_cg
 from sklearn.metrics import accuracy_score
 
-#import matplotlib
+# import matplotlib
 
 # Load data Set
 data = scipy.io.loadmat('ex3data1.mat')
@@ -23,13 +23,13 @@ mtrain = np.float(Xtrain.shape[0]) # Number of training examples
 mtest = np.float(Xtest.shape[0]) # Number of testing examples
 n = np.float(X.shape[1]) # Number of features
 Output = 10 # Output layer dimensionality - here the system is binary so we want two output layers. We could also use one, I suppose.
-Lambda = 0.001 # Regularization parameter, anything >0.26942 causes linear decision boundary and < 0.0003 causes fail max iterations
+Lambda = 0.01 # Regularization parameter, anything >0.26942 causes linear decision boundary and < 0.0003 causes fail max iterations
 Hidden = 10 # Number of hidden layers
-ytrain_matrix = np.eye(Output)[ytrain]
-ytest_matrix = np.eye(Output)[ytest]
+ytrain_matrix = np.eye(Output)[ytrain].reshape(mtrain,Output)
+ytest_matrix = np.eye(Output)[ytest].reshape(mtest,Output)
 
 # Visualize the data
-def plot(draw):
+def drawplot(draw):
     if draw:
         idx = np.random.randint(m,size=100)
         fig, ax = plt.subplots(10, 10)
@@ -40,7 +40,7 @@ def plot(draw):
                 ax[i, j].set_axis_off() # Turns off the axes for all the subplots
                 ax[i,j].imshow(Xi, aspect='auto',cmap='gray')
         plt.show()
-plot(False)
+drawplot(False)
 
 def sigmoid(z):
     return 1.0 / (1.0 + np.exp(-1 * z))
@@ -64,25 +64,25 @@ def predict(theta,x):
 def nn(theta):
 
     # Unroll thetas
-    theta1 = theta[:(n+1)*Hidden].reshape((Hidden,(n+1))) # [Hiddden X n + 1]
-    theta2 = theta[(n+1)*Hidden:].reshape((Output,(Hidden+1))) # [Output X Hidden + 1]
+    theta1 = theta[:(n+1)*Hidden].reshape((Hidden,(n+1))) # [Hiddden X n + 1] = [10X401]
+    theta2 = theta[(n+1)*Hidden:].reshape((Output,(Hidden+1))) # [Output X Hidden + 1] = [10X11]
 
     # Forward propagation
-    a1 = np.concatenate((np.ones((1,mtrain)).T,Xtrain),axis=1) # [m X n + 1] = [200X3]
-    z2 = a1.dot(theta1.T) # [m X n + 1] = [200X3]
-    a2 = np.concatenate((np.ones((1,mtrain)).T,sigmoid(z2)),axis=1) # [m X Hidden + 1] = [200X4] (the bias unit is also added here, hence the + 1!)
-    z3 = a2.dot(theta2.T) # [m X Output] = [200X2]
-    a3 = sigmoid(z3) # [m X Output] = [200X2]
+    a1 = np.concatenate((np.ones((1,mtrain)).T,Xtrain),axis=1) # [m X n + 1] = [3750X401]
+    z2 = a1.dot(theta1.T) # [m X Hidden] = [3750X10]
+    a2 = np.concatenate((np.ones((1,mtrain)).T,sigmoid(z2)),axis=1) # [m X Hidden + 1] = [3750X11]
+    z3 = a2.dot(theta2.T) # [m X Output] = [3750X10]
+    a3 = sigmoid(z3) # [m X Output] = [3750X10]
 
     # Cost function
     J = np.sum(-ytrain_matrix*np.log(a3) - (1-ytrain_matrix)*(np.log(1-a3)))/mtrain + ((Lambda/(2*mtrain)) * (np.sum(theta1[:,1:]**2) + np.sum(theta2[:,1:]**2)))
         
 
     # Backpropagation
-    d3 = (a3 - ytrain_matrix) # [m X Output] = [200X2]
-    d2 = (a3 - ytrain_matrix).dot(theta2) * (a2*(1-a2)) # [m X Hidden] = [200X3]
-    D1 = d2[:,1:].T.dot(a1)/mtrain # [Hidden X n + 1] = [3X3]
-    D2 = d3.T.dot(a2)/mtrain # [n X Hidden + 1] = [2X4]
+    d3 = (a3 - ytrain_matrix) # [m X Output] = [3750X10]
+    d2 = (a3 - ytrain_matrix).dot(theta2) * (a2*(1-a2)) # [m X Hidden+1] = [3750X11]
+    D1 = d2[:,1:].T.dot(a1)/mtrain # [Hidden X n + 1] = [10X401]
+    D2 = d3.T.dot(a2)/mtrain # [n X Hidden + 1] = [10X11]
     if Lambda!=0:
         D1[:,1:] += (Lambda/mtrain * theta1[:,1:]) # [Hidden X n + 1] = [3X3]
         D2[:,1:] += (Lambda/mtrain * theta2[:,1:]) # [Output X Hidden + 1] = [2X4]
@@ -107,7 +107,7 @@ def randtheta(L_in,L_out):
 
 theta_init = np.concatenate((randtheta(n,Hidden).reshape(((n+1)*Hidden)),randtheta(Hidden,Output).reshape(((Hidden+1)*Output))))
 
-opt = fmin_cg(cost, x0=theta_init, fprime=grad, full_output=1, disp=1);
+opt = fmin_cg(cost, x0=theta_init, fprime=grad, full_output=1, disp=1, maxiter=300);
 theta = opt[0]
 print "The cost is %f" %(opt[1])
 print "The accuracy is on the training set is %f" %(accuracy_score(ytrain, predict(theta,Xtrain))) # this comes from sklearn.metrics
